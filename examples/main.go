c96c388dc890f1581fbd87cef34fc0c595a47179
Simone Tesini <simone.tesini@siag.it>
2024-07-09 10:53:19 +0200

    add field setting and casting

--- examples/main.go
+++ examples/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/Tesohh/goat"
+import (
+	"github.com/Tesohh/goat"
+)
 
 type Z struct {
 	x1 int
@@ -10,10 +12,21 @@ type Z struct {
 type TestGoatType struct {
 	X int    `goat:"x"`
 	Y string `goat:"y,query"`
-	Z
+	// Z
 }
 
 func main() {
-	route := goat.Route[TestGoatType, string]{}
-	route.MakeHandlerFunc(&goat.Server{})
+	route := goat.Route[TestGoatType, string]{
+		Path:               "/tubre",
+		Method:             "GET",
+		Description:        "Tubreeee",
+		ParamsDescriptions: map[string]string{},
+		Handler: func(c *goat.Context[TestGoatType]) (int, *string, error) {
+			var s = "wfw"
+			return 200, &s, nil
+		},
+	}
+	server := goat.NewServer()
+	server.AddController(route)
+	server.Listen(":8080")
 }
